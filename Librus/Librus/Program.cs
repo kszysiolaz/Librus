50952291eb489357800154f1944681b9312efd62
kszysiolaz <129276782+kszysiolaz@users.noreply.github.com>
2023-03-29 13:04:58 +0200

    main

---
+++ Librus/Librus/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Librus {
+internal class Program {
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+}
