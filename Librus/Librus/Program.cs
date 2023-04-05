6d39af13f29ec213e67b1a9b547de74bfc847530
cwel <chanko.szymon@gmail.com>
2023-04-05 11:54:07 +0200

    main

--- Librus/Librus/Program.cs
+++ Librus/Librus/Program.cs
@@ -1,10 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
+using System.Data.SqlClient;
 
-namespace Librus;
-internal class Program {
-    static void Main()
+namespace Librus
+{
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            // Utwórz połączenie z bazą danych
+            string connectionString = "Data Source=(local);Initial Catalog=MyDatabase;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                // Otwórz połączenie
+                connection.Open();
+
+                // Utwórz obiekt polecenia
+                SqlCommand command = new SqlCommand("SELECT * FROM Customers", connection);
+
+                // Wykonaj zapytanie i odczytaj wyniki
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["CustomerName"]);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Zamknij połączenie
+                connection.Close();
+            }
+
+            Console.ReadKey();
+        }
     }
 }
